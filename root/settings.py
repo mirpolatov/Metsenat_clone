6b149a489afb24f8a370f100eba516689aad94af
miraziz <mirazizmirpolatov9@gmail.com>
2023-10-21 11:37:49 +0500

    static

--- root/settings.py
+++ root/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'django_filters',
     'storages',
 
-
 ]
 
 MIDDLEWARE = [
@@ -124,12 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
